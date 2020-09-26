634d1714fe6d872889b6c3a8b82e6d2cfcf8ee1d
pigs-pigs <potterpiggy13@outlook.com>
2020-09-26 10:29:45 -0400

    commit

--- src/init.js
+++ src/init.js
@@ -1,16 +1,32 @@
-const settings ={
-    canvas: "game1",
-    character1: "img.png"
+import "./jquery.js";
 
+const MOBSO = {
+  canvas: $(),
+  character1: null,
+  character2: null,
+  //currency: null,
+};
+const settings = {
+  canvas: "mobso-game",
+  //character1: "img.png",
+};
+
+function initMOBSO(settingsParam) {
+  MOBSO.canvas = $("#" + settingsParam.canvas);
+ // MOBSO.character1 = settingsParam.character1;
+  //MOBSO.character2 = settingsParam.character2;
+  //Create Bar space, character spaces, main frame, currency frame
 }
 
-function initMOBSO(settings) {
- var canvas = document.getElementById(settings.canvas)
- var character1 = settings.character1
+function makeBar(textParam, idParam, charParam) {
+  var bar = $("<div></div>");
+  bar
+    .attr({
+      id: "textBar" + idParam,
+      style: "background: black; color: white; height: 100px;",
+    })
+    .text(textParam)
+    .appendTo(MOBSO.canvas);
 }
-
-function makeBar(text,num,char){
-    //make a div/box
-    //add text
-    //show char
-}
+initMOBSO(settings);
+makeBar("hi", 8, character1);
